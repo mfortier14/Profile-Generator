a7298245868146257ff3241478143fbc14ce6f6e
Marissa Fortier <marissafortier@gmail.com>
2019-11-23 10:13:15 -0500

    added the api call and the code to convert to a pdf

--- index.js
+++ index.js
@@ -19,3 +19,41 @@ const question = [
         choices: ["blue", "green", "pink", "yellow", "red"]
     }
 ];
+
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+    inquirer.prompt(question).then(({ github, color }) => {
+        console.log("searching....");
+
+        // api
+        //     .gitUser(github)
+        //     .then(response =>
+        //         api.getTotalStars(github).then(stars => {
+        //             return generateHTML({
+        //                 stars,
+        //                 color,
+        //                 ...response.data
+        //             });
+        //         })
+        //     )
+        //     .then(html => {
+                const conversion = convertFactory({
+                    convertPath: convertFactory.converters.PDF
+                });
+
+                // conversion({ html }, function (err, result) {
+                //     if (err)
+                //         throw err;
+                //     result.stream.pipe(
+                //         fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+                //     );
+                //     conversion.kill();
+                // });
+                // open(path.join(process.cwd(), "resume.pdf"));
+            // });
+    });
+}
+init();
